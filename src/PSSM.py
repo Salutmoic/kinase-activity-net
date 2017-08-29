85cb9fa2ea6e4823b7558c178db1a2b76d7edbda
Borgthor Petursson <borgthor@ebi.ac.uk>
2017-08-29 14:35:39 +0000

    Replace PSSM.py, pssm matrices and ideas how edges can be assessed by using them

--- src/PSSM.py
+++ src/PSSM.py
@@ -1,8 +1,5 @@
-
 import numpy as np
 import random
-
-
 
 def read_data():
     #Reads kinase-substrate table where substrate and kinase are found in 
@@ -33,8 +30,9 @@ def amino_acids():
 
 
 def pmms(ks):
-    #
-    #
+    # returns a dictionary where keys are kinases and values are pmms matrices made from the known substrates
+    # for each kinase
+    
     aminos = amino_acids()
     pmms = {}
 
@@ -50,7 +48,7 @@ def pmms(ks):
                     cmat[k][aminos[j[k]]] = cmat[k][aminos[j[k]]] +1
                 
             
-
+        #I use 0.01 as pseudocount 
         cmat = cmat + 0.01*len(seqs)
         cmat = cmat/len(seqs)
         pmms[i] = cmat
@@ -60,7 +58,8 @@ def pmms(ks):
 
 
 def subseqs():
-    #This function extracts peptides surrounding phospho sites
+    #This function extracts peptides surrounding phospho sites found in the phosphosite plus
+    # human_kinase_table
     f = open("/home/borgthor/Kinase_activities/data/human_kinase_table","r")
     
     columns = f.readline()
@@ -91,12 +90,9 @@ def score(seq,pmms):
     return score
         
         
-    
-
-
-
 def dist(seqs,pmms):
-    #creates distribution of 
+    #creates distribution of pmms scores
+    #scores are calculated by scoring a random phosphosite relative to to kinase i
     dist = {}
 
     for i in pmms:
@@ -111,22 +107,87 @@ def dist(seqs,pmms):
 
     return dist
         
+def kin_phosphosites(ks):
+    #returns dictionary with every known phosphosite found on each kinase in the
+    #Ochoa et al. dataset
+    
+    f =  open("/home/borgthor/phosphosites_reduced.txt")
+    psites = {}
+    columns = f.readline()
 
+    columns =columns.split("\t")
+    seqind = columns.index("SITE_...7_AA")
+    
+    for line in f:
+        
+        line = line.split("\t")
+        if line[0] not in psites:
+            psites[line[0]] = [line[seqind]]
+        else:
+            psites[line[0]].append(line[seqind])
+
+    return psites
+
+def score_kin_pairs(ks):
+    # loads a netwoek of kinase-> kinase interactions and scores them with the pssm
+    # it returns a dictionary with kinasa A and kinase B and list of scores for each phosphosite found
+    # on B
+
+    #f is some imaginary network
+    f =  open("/home/borgthor/network.txt")
+    psites = kin_phosphosites(ks)
+    pmms = pmms(ks)
+    scores = {}
+    for line in f:
+        line = line.split("\t")
+        A = line[0]
+        B= line[1]
+
+        sites = psites[B]
+        for i in sites:
+            sc = score(i,pmms[A])
+            if (A,B) in scores:
+               scores[(A,B)] = [sc]
+            else:
+                scores[(A,B)].append(sc)
+
+    return scores
+
+
+def assess_edges(scores,dist):
+    #Finds the highest scoreing phosphosite for ech A,B pair and compares it to the
+    #distribution generated for B (A-> B) from the dist() function
+    pvals = {}
+
+    for i in scores.keys():
+        m = max(scores[i])
+        p = np.percentile(dist[i[1]],m)
+        pvals[i] = p
+
+    return pvals
     
     
-    
-
-
+               
 t,ks = read_data()
 
 pmms = pmms(ks)
 
 seqs = subseqs()
 
+score = score_kin_pairs(ks)
+
 dist = dist(seqs,pmms)
+
+psites = kin_phosphosites(ks)
+
+pvals = assess_edges(scores,dist)
+
 np.save('/home/borgthor/Kinase_activities/cons_matrices', pmms) 
-np.save('/home/borgthor/Kinase_activities/score_distributions', dist) 
-    
+np.save('/home/borgthor/Kinase_activities/score_distributions', dist)
+np.save('/home/borgthor/Kinase_activities/kinase pairs score', score) 
+np.save('/home/borgthor/Kinase_activities/phosphosites per kinases', psites) 
+np.save('/home/borgthor/Kinase_activities/pcals for each kinase pair', pavals) 
+
 read_dictionary = np.load('/home/borgthor/Kinase_activities/cons_matrices.npy').item()    
     
 
