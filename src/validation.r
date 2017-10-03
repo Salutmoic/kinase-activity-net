af4a5d74e5d642b0007c8fa47421fb740cab880b
Brandon Invergo <brandon@invergo.net>
2017-10-03 13:12:06 +0100

    fix validation script

--- src/validation.r
+++ src/validation.r
@@ -11,28 +11,22 @@ val.set.file <- argv[2]
 pred.score <- read.delim(pred.score.file, as.is=TRUE)
 rownames(pred.score) <- paste(pred.score[,1], pred.score[,2], sep="-")
 
-true.intxns.tbl <- read.table(val.set.file)
+true.intxns.tbl <- read.table(val.set.file, as.is=TRUE)
 true.intxns <- paste(true.intxns.tbl[,1], true.intxns.tbl[,2], sep="-")
 true.intxns <- true.intxns[which(true.intxns %in% rownames(pred.score))]
+
+all.kinases <- unique(c(true.intxns.tbl[,1], true.intxns.tbl[,2]))
+possible.intxns <- as.vector(outer(all.kinases, all.kinases, paste, sep="-"))
+possible.false.intxns <- setdiff(possible.intxns, true.intxns)
 
 pred.data <- NULL
 label.data <- NULL
 
+n <- 50
+
 ## This procedure is sufficient for training-free predictions.
-for (i in 1:100){
-    false.intxns <- c()
-    ## Build up a set of random interactions that are not in the
-    ## true-interaction set and have a prediction score
-    while (length(false.pos.intxns) < 3*length(true.pos.intxns)){
-        false.intxn.pair <- c(true.intxns.tbl[sample(1:nrow(true.intxns.tbl)), 1],
-                              true.intxns.tbl[sample(1:nrow(true.intxns.tbl)), 2])
-        if (false.intxn.pair[1] == false.intxn.pair[2])
-            next
-        false.intxn <- paste(false.intxn.pair[1], false.intxn.pair[2], sep="-")
-        if (false.intxn %in% false.intxns || !(false.intxn %in% rownames(pred.score)))
-            next
-        false.intxns <- c(false.intxns, false.intxn)
-    }
+for (i in 1:n){
+    false.intxns <- sample(possible.false.intxns, size=3*length(true.intxns))
     ## Put together the tables of predictions and TRUE/FALSE labels
     intxns <- c(true.intxns, false.intxns)
     preds <- pred.score[intxns, 3]
@@ -59,6 +53,8 @@ par(cex=1.25)
 perf.roc <- performance(pred, measure="tpr", x.measure="fpr")
 perf.auc <- performance(pred, "auc")
 mean.auc <- mean(unlist(perf.auc@y.values))
+se.auc <- sd(unlist(perf.auc@y.values))/sqrt(n)
+message(paste("Mean AUC =", mean.auc, "S.E.M. =", se.auc))
 plot(perf.roc, avg="vertical",
      main=paste(data.basename, paste0("Mean AUC=", mean.auc), sep="\n"))
 
