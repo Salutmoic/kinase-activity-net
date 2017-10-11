cd663ffd79c08d7d98578f1e3369e85570333372
Borgthor Petursson <borgthor@ebi.ac.uk>
2017-10-11 12:48:59 +0000

    Replace log_reg.r

--- src/log_reg.r
+++ src/log_reg.r
@@ -1,7 +1,8 @@
+
 #Use logistic regression to test and predict kinase pairs
 
 
-library(glm)
+library(glm2)
 
 argv <- commandArgs(TRUE)
 if (length(argv) != 2){
@@ -15,9 +16,13 @@ if (length(argv) != 2){
 train <- argv[1]
 test <- argv[2]
 
+#take part of the training set (known outcomes) to cross validate the regression model 
+train.valid = train[(round(0.75*nrow(train))):nrow(train),]
+train = train[1:(round(0.75*nrow(train))),]
+
 train <- function(train){
 
-  model = glm(train[,3]~train[,4]+train[,5]+train[,6],family = binomial()) 
+  model = glm(train[,3]~train[,4]+train[,5]+train[,6],family = gaussian()) 
 
   
   return(model)  
@@ -28,11 +33,20 @@ test <- function(test,train){
   
   model = train(train)
 
-  predition = predict(model,test[,3:5], type = "response")
+  predition = predict(model,test[,3:5], type = response)
   residuals = predict(model,test[,3:5], type = "deviance")
 
   test = cbind(test,prediction,residuals)
   return(test)
+}
+
+#test performance of the model on the train.valid data frame
+
+model.assecc <-function(train.valid,train){
+  model = train(train)
+  predition = predict(model,train.valid[,4:6], type = response)
+  residuals = predict(model,train.valid[,4:6], type = "deviance")
+  return(confusionMatrix(prediction,train.valid[,3]))
 }
 
 test.res = test(test,train)
