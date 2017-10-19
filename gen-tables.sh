331c0e6b418e7b2fcb266763095c7a75b80c2bb0
Brandon Invergo <brandon@invergo.net>
2017-10-19 12:29:10 +0100

    small clean-up

--- gen-tables.sh
+++ gen-tables.sh
@@ -8,5 +8,5 @@ mkdir -p $LOGDIR
 for s in $TABLE_STRATEGIES; do
     make clean-data TABLE_STRATEGY=$s
     bsub -o ${LOGDIR}/gen-table-${s}.out -e ${LOGDIR}/gen-table-${s}.err make data \
-         TABLE_STRATEGY=$s KSEA_MIN_SITES=5 ENTROPY_FILTER=0.5 NA_THRESHOLD=0.3333
+         TABLE_STRATEGY=$s
 done
