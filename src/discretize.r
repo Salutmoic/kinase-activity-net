209edc841a059556f5b0bd883c148c369f282f26
Brandon Invergo <brandon@invergo.net>
2017-10-19 14:18:35 +0100

    add another experimental discretization method

--- src/discretize.r
+++ src/discretize.r
@@ -62,12 +62,27 @@ discr.trunc <- function(kin.act.m){
     return(discrete)
 }
 
+discr.even <- function(kin.act.m){
+    ## Cluster the data evenly into 5 bins
+    num.clusts <- 5
+    num.conds <- ncol(kin.act.m)
+    clusts <- sort(rep(1:num.clusts, ceiling(num.conds/num.clusts))[1:num.conds])
+    discrete <- t(as.matrix(unlist(apply(kin.act.m, 1,
+                                         function(m.row){
+                                             clusts[order(order(m.row, decreasing=FALSE), decreasing=FALSE)]
+                                         }))))
+    rownames(discrete) <- rownames(kin.act.m)
+    colnames(discrete) <- colnames(kin.act.m)
+    return(discrete)
+}
+
 discretize <- function(kin.act.m, method){
     switch(method,
         mclust.whole = mclust.whole(kin.act.m),
         mclust.by.row = mclust.by.row(kin.act.m),
         manual = discr.manual(kin.act.m),
-        trunc = discr.trunc(kin.act.m))
+        trunc = discr.trunc(kin.act.m),
+        even = discr.even(kin.act.m))
 }   
 
 argv <- commandArgs(TRUE)
@@ -79,6 +94,10 @@ method <- argv[1]
 data.file <- argv[2]
 out.file <- argv[3]
 
+if (!(method %in% c("mclust.whole", "mclust.by.row", "manual", "trunc", "even"))){
+    stop("Invalid method name")
+}
+
 kin.act.tbl <- read.table(data.file, as.is=TRUE)
 names(kin.act.tbl) <- c("kinase", "condition", "activity")
 
@@ -86,6 +105,8 @@ kin.act.m <- acast(kin.act.tbl, kinase ~ condition)
 discrete <- discretize(kin.act.m, method)
 
 discrete.tbl <- melt(discrete)
+names(discrete.tbl) <- c("kinase", "condition", "activity")
 
-write.table(discrete.tbl, out.file, row.names=FALSE,  col.names=FALSE,
+write.table(discrete.tbl[order(discrete.tbl$kinase, discrete.tbl$condition),],
+            out.file, row.names=FALSE,  col.names=FALSE,
             sep="\t", quote=FALSE)
