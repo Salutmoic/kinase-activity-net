c4f965babdcc432c2bd570e79215bd5cfb71d8e7
Brandon Invergo <brandon@invergo.net>
2017-12-08 16:10:04 +0000

    update BART evaluation scripts

--- src/bart-eval.r
+++ src/bart-eval.r
@@ -1,21 +1,24 @@
 suppressPackageStartupMessages(library(data.table))
-options(java.parameters = "-Xmx24g")
+options(java.parameters = "-Xmx32g")
 suppressPackageStartupMessages(library(bartMachine))
 set_bart_machine_num_cores(24)
 suppressPackageStartupMessages(library(ROCR))
 
 argv <- commandArgs(TRUE)
-if (length(argv) != 3){
-    stop("USAGE: <script> MERGED_PREDS VAL_SET NEG_VAL_SET")
+if (length(argv) != 5){
+    stop("USAGE: <script> MERGED_PREDS VAL_SET NEG_VAL_SET RAND_NEGS CLASSIFIER")
 }
 
 merged_pred_file <- argv[1]
 val_set_file <- argv[2]
 neg_val_set_file <- argv[3]
+use_rand_negs <- as.logical(argv[4])
+classifier <- as.logical(argv[5])
 
 merged_pred <- read.delim(merged_pred_file, as.is=TRUE)
 names(merged_pred)[1:2] <- c("prot1", "prot2")
 merged_pred <- subset(merged_pred, prot1 != prot2)
+merged_pred <- merged_pred[complete.cases(merged_pred),]
 rownames(merged_pred) <- paste(merged_pred$prot1, merged_pred$prot2, sep="-")
 
 true_intxns_tbl <- read.table(val_set_file, as.is = TRUE)
@@ -23,32 +26,29 @@ possible_true_intxns <- paste(true_intxns_tbl[, 1], true_intxns_tbl[, 2],
                               sep = "-")
 possible_true_intxns <- intersect(possible_true_intxns, rownames(merged_pred))
 
-false_intxns_tbl <- read.table(neg_val_set_file, as.is = TRUE)
-possible_false_intxns <- paste(false_intxns_tbl[, 1], false_intxns_tbl[, 2],
-                               sep = "-")
-possible_false_intxns <- intersect(possible_false_intxns, rownames(merged_pred))
-
 ## Generate some reverse true interactions to be mixed in as negatives
-rev_true_intxns <- paste(true_intxns_tbl[, 2], true_intxns_tbl[, 1], sep = "-")
-rev_true_intxns <- intersect(rev_true_intxns, rownames(merged_pred))
-rev_true_intxns <- setdiff(rev_true_intxns, possible_true_intxns)
-rev_true_intxns <- setdiff(rev_true_intxns, possible_false_intxns)
-
-num_rev_true <- 0.5 * length(rev_true_intxns)
-sample_size <- round(0.8 * min(length(possible_false_intxns) + num_rev_true,
+if (use_rand_negs){
+    rand_neg_intxns <- rownames(merged_pred)
+    possible_false_intxns <- setdiff(rand_neg_intxns, possible_true_intxns)
+}else{
+    false_intxns_tbl <- read.table(neg_val_set_file, as.is = TRUE)
+    possible_false_intxns <- paste(false_intxns_tbl[, 1], false_intxns_tbl[, 2],
+                                   sep = "-")
+    possible_false_intxns <- intersect(possible_false_intxns, rownames(merged_pred))
+}
+sample_size <- round(0.8 * min(length(possible_false_intxns),
                                length(possible_true_intxns)))
 
-num_reps <- 5
+print(possible_false_intxns)
+
+num_reps <- 50
 k <- 5
 all_probs <- NULL
 all_labels <- NULL
 
 for (n in 1:num_reps){
     message(paste("Rep.", n))
-    false_intxns <- sample(union(possible_false_intxns,
-                                 sample(rev_true_intxns,
-                                        size=as.integer(num_rev_true))),
-                                 size=sample_size)
+    false_intxns <- sample(possible_false_intxns, size=sample_size)
     true_intxns <- sample(possible_true_intxns, size=sample_size)
     ## Put together the tables of predictions and TRUE/FALSE labels
     intxns <- c(true_intxns, false_intxns)
@@ -74,23 +74,34 @@ for (n in 1:num_reps){
         val_labels <- labels[val_rows]
         train_set <- preds[train_rows,]
         train_labels <- labels[train_rows]
+        if (classifier){
+            val_labels <- as.factor(val_labels)
+            train_labels <- as.factor(train_labels)
+        }else{
+            val_labels <- as.integer(val_labels)
+            train_labels <- as.integer(train_labels)
+        }
+        print(head(cbind(train_set, train_labels)))
         bart <- bartMachineCV(train_set,
-                              as.integer(train_labels),
-                              use_missing_data=FALSE,
+                              train_labels,
+                              use_missing_data=TRUE,
                               replace_missing_data_with_x_j_bar=FALSE,
                               impute_missingness_with_x_j_bar_for_lm=FALSE,
                               verbose=FALSE,
                               serialize=FALSE)
-        val_preds <- bart_predict_for_test_data(bart, val_set, as.integer(val_labels))
-        ## neg_val_preds <- which(val_preds$y_hat == FALSE)
-        ## val_preds$p_hat[neg_val_preds] <- 1.0 - val_preds$p_hat[neg_val_preds]
+        val_preds <- bart_predict_for_test_data(bart, val_set, val_labels)
+        if (classifier){
+            neg_val_preds <- which(val_preds$y_hat == FALSE)
+            val_preds$p_hat[neg_val_preds] <- 1.0 - val_preds$p_hat[neg_val_preds]
+            val_preds <- val_preds$p_hat
+        }else{
+            val_preds <- val_preds$y_hat
+        }
         if (is.null(all_probs)){
-            ## all_probs <- val_preds$p_hat
-            all_probs <- val_preds$y_hat
+            all_probs <- val_preds
             all_labels <- val_labels
         }else{
-            ## all_probs <- cbind(all_probs, val_preds$p_hat)
-            all_probs <- cbind(all_probs, val_preds$y_hat)
+            all_probs <- cbind(all_probs, val_preds)
             all_labels <- cbind(all_labels, val_labels)
         }
     }
@@ -102,7 +113,17 @@ rocr_auc <- performance(rocr_pred, "auc")
 mean_auc <- mean(unlist(rocr_auc@y.values), na.rm=TRUE)
 se_auc <- sd(unlist(rocr_auc@y.values), na.rm=TRUE)/sqrt(num_reps*k)
 
-pdf("img/bartmachine.pdf")
+file_base <- strsplit(basename(merged_pred_file), split="\\.")[[1]][1]
+img_file <- paste0(file_base, "-bart")
+if (classifier){
+    img_file <- paste(img_file, "classifier",  sep="-")
+}
+if (use_rand_negs){
+    img_file <- paste(img_file, "rand-negs", sep="-")
+}
+img_file <- paste0("img/", img_file, "-roc.pdf")
+
+pdf(img_file)
 plot(rocr_roc, spread.estimate="boxplot", avg="vertical",
      main=paste0("BART\n(mean AUC=", format(mean_auc, digits=2),
                  ", S.E.M=", format(se_auc, digits=2), ")"))
