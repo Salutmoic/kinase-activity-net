1f30ca1cec953c47fba746c5e11ec9520a9f1093
Brandon Invergo <brandon@invergo.net>
2018-01-24 10:13:21 +0000

    use functional score in regulatory site correlations

--- src/reg-site-cor.r
+++ src/reg-site-cor.r
@@ -4,36 +4,53 @@ load("data/external/esetNR.Rdata")
 
 cond.anno <- pData(esetNR)
 phospho.anno <- fData(esetNR)
-phospho.vals <- exprs(esetNR)
+phospho.vals.full <- exprs(esetNR)
 
 cptac.start <- 1604
 cptac.end <- 1711
 cptac.range <- 1604:1711
 cptac.conds <- paste(as.character(cptac.range), "290", sep="_")
-phospho.vals <- phospho.vals[, -which(colnames(phospho.vals) %in% cptac.conds)]
+phospho.vals.sub <- phospho.vals.full[, -which(colnames(phospho.vals.full) %in% cptac.conds)]
 
-reg.sites.full <- read.delim("data/reg_sites.tsv", as.is=TRUE)
-reg.sites.full$MOD_RSD <- sub("^[STY]", "", reg.sites.full$MOD_RSD)
-reg.sites <- reg.sites.full[which(grepl("enzymatic activity, induced",
-                                        reg.sites.full$ON_FUNCTION) |
-                                  grepl("enzymatic activity, inhibited",
-                                        reg.sites.full$ON_FUNCTION)),]
+all.kins <- read.table("data/human-kinome.txt", as.is=TRUE)[,1]
+
+ensp.id.map.full <- read.table("data/ensembl-id-map.tsv", as.is=TRUE)
+names(ensp.id.map.full) <- c("ensembl", "gene.name")
+ensp.id.map <- subset(ensp.id.map.full, gene.name %in% all.kins)
+rownames(ensp.id.map) <- ensp.id.map$ensembl
+
+phospho.vals.ensp <- unlist(lapply(strsplit(rownames(phospho.vals.sub), split="_"),
+                                   function(foo) foo[1]))
+phospho.vals <- phospho.vals.sub[which(phospho.vals.ensp %in% ensp.id.map$ensembl),]
+phospho.vals.ensp <- unlist(lapply(strsplit(rownames(phospho.vals), split="_"),
+                                   function(foo) foo[1]))
+phospho.vals.pos <- unlist(lapply(strsplit(rownames(phospho.vals), split="_"),
+                                  function(foo) foo[2]))
+phospho.vals.gene <- ensp.id.map[phospho.vals.ensp, "gene.name"]
+
+rownames(phospho.vals) <- paste(phospho.vals.gene, phospho.vals.pos, sep="_")
+
+reg.sites <- read.delim("data/reg_sites.tsv", as.is=TRUE)
+reg.sites$MOD_RSD <- sub("^[STY]", "", reg.sites$MOD_RSD)
+## reg.sites <- reg.sites.full[which(grepl("enzymatic activity, induced",
+##                                         reg.sites.full$ON_FUNCTION) |
+##                                   grepl("enzymatic activity, inhibited",
+##                                         reg.sites.full$ON_FUNCTION)),]
+rownames(reg.sites) <- paste(reg.sites$GENE, reg.sites$MOD_RSD, sep="_")
 
 ## activity induction => 1, activity inhibition => -1
-reg.sites$direction <- as.integer(grepl("induced", reg.sites$ON_FUNCTION))*2 - 1
+## reg.sites$direction <- as.integer(grepl("induced", reg.sites$ON_FUNCTION))*2 - 1
 
 
-reg.sites.ensp.full <- merge(phospho.anno, reg.sites,
-                             by.x=c("gene_name", "positions"),
-                             by.y=c("GENE", "MOD_RSD"))
-rownames(reg.sites.ensp.full) <- paste(reg.sites.ensp.full$ensp,
-                                       reg.sites.ensp.full$positions,
-                                       "P", sep="_")
+phosfun <- read.delim("data/phosfun.tsv", as.is=TRUE)
+names(phosfun) <- c("prot", "pos", "score")
+rownames(phosfun) <- paste(phosfun$prot, phosfun$pos, sep="_")
+for (site in rownames(phosfun)){
+    if (site %in% rownames(reg.sites))
+        phosfun[site, "score"] <- 1.0
+}
+med.phosfun <- median(phosfun$score)
 
-reg.sites.i <- which(rownames(reg.sites.ensp.full) %in% rownames(phospho.vals))
-reg.sites.ensp <- reg.sites.ensp.full[reg.sites.i,]
-
-all.kins <- unique(reg.sites.ensp$gene_name)
 kin.pairs <- expand.grid(all.kins, all.kins, stringsAsFactors=FALSE)
 kin1s <- c()
 kin2s <- c()
@@ -42,24 +59,42 @@ cors <- c()
 for (i in 1:nrow(kin.pairs)){
     kin1 <- kin.pairs[i, 1]
     kin2 <- kin.pairs[i, 2]
-    kin1.reg.sites <- subset(reg.sites.ensp, gene_name==kin1)
-    kin2.reg.sites <- subset(reg.sites.ensp, gene_name==kin2)
+    if (kin1 == kin2 || !(kin1 %in% phospho.vals.gene) ||
+        !(kin2 %in% phospho.vals.gene)){
+        kin1s <- c(kin1s, kin1)
+        kin2s <- c(kin2s, kin2)
+        p.vals <- c(p.vals, NA)
+        next
+    }
+    kin1.sites <- which(phospho.vals.gene == kin1)
+    kin2.sites <- which(phospho.vals.gene == kin2)
     pair.p.vals <- c()
     ## best.p.value <- -Inf
-    for (kin1.ensp in rownames(kin1.reg.sites)){
-        for (kin2.ensp in rownames(kin2.reg.sites)){
-            kin1.ensp.dir <- reg.sites.ensp[kin1.ensp, "direction"]
-            kin2.ensp.dir <- reg.sites.ensp[kin2.ensp, "direction"]
-            kin1.meas.conds <- which(!is.na(phospho.vals[kin1.ensp,]))
-            kin2.meas.conds <- which(!is.na(phospho.vals[kin2.ensp,]))
+    for (kin1.site in kin1.sites){
+        for (kin2.site in kin2.sites){
+            kin1.meas.conds <- which(!is.na(phospho.vals[kin1.site,]))
+            kin2.meas.conds <- which(!is.na(phospho.vals[kin2.site,]))
             shared.conds <- intersect(kin1.meas.conds, kin2.meas.conds)
-            if (length(shared.conds) < 3)
+            if (length(shared.conds) < 5)
                 next
-            kin1.ensp.phospho <- phospho.vals[kin1.ensp,] * kin1.ensp.dir
-            kin2.ensp.phospho <- phospho.vals[kin2.ensp,] * kin2.ensp.dir
-            ct <- cor.test(kin1.ensp.phospho, kin2.ensp.phospho, method="spearman",
+            kin1.phosfun.i <- rownames(phospho.vals)[kin1.site]
+            kin2.phosfun.i <- rownames(phospho.vals)[kin2.site]
+            if (!(kin1.phosfun.i %in% rownames(phosfun))){
+                kin1.phosfun <- med.phosfun
+            }else{
+                kin1.phosfun <- phosfun[kin1.phosfun.i, "score"]
+            }
+            if (!(kin2.phosfun.i %in% rownames(phosfun))){
+                kin2.phosfun <- med.phosfun
+            }else{
+                kin2.phosfun <- phosfun[kin2.phosfun.i, "score"]
+            }
+            kin1.phospho <- phospho.vals[kin1.site,]
+            kin2.phospho <- phospho.vals[kin2.site,]
+            ct <- cor.test(kin1.phospho, kin2.phospho, method="spearman",
                            use="pairwise.complete.obs")
-            p.value <- -log10(ct$p.value)
+            ## p.value <- -log10(ct$p.value/(kin1.phosfun*kin2.phosfun))
+            p.value <- -log10(ct$p.value)*kin1.phosfun*kin2.phosfun
             if (!is.infinite(p.value)){ ## && p.value > best.p.value){
                 ## best.p.value <- p.value
                 ## best.cor.est <- ct$estimate
@@ -67,10 +102,6 @@ for (i in 1:nrow(kin.pairs)){
             }
         }
     }
-    ## if (is.infinite(best.p.value)){
-    ##     best.p.value <- NA
-    ##     best.cor.est <- NA
-    ## }
     if (length(pair.p.vals) == 0){
         p.val <- NA
     }else{
@@ -80,9 +111,7 @@ for (i in 1:nrow(kin.pairs)){
         p.val <- NA
     kin1s <- c(kin1s, kin1)
     kin2s <- c(kin2s, kin2)
-    ## p.vals <- c(p.vals, best.p.value)
     p.vals <- c(p.vals, p.val)
-    ## cors <- c(cors, best.cor.est)
 }
 
 results <- data.frame(node1=kin1s, node2=kin2s, reg.site.cor.p=p.vals)
@@ -90,6 +119,8 @@ results$reg.site.cor.p <- results$reg.site.cor.p/max(results$reg.site.cor.p, na.
 
 write.table(results[order(results$node1),], "out/reg-site-cor.tsv", quote=FALSE,
             row.names=FALSE, col.names=TRUE, sep="\t")
+
+message("Done with cor, starting cor^2")
 
 ## kin.cor <- acast(results, node1 ~ node2)
 kin.cor <- matrix(p.vals, nrow=length(all.kins))
@@ -102,6 +133,12 @@ p.vals <- c()
 for (i in 1:nrow(kin.pairs)){
     kin1 <- kin.pairs[i, 1]
     kin2 <- kin.pairs[i, 2]
+    if (kin1 == kin2){
+        kin1s <- c(kin1s, kin1)
+        kin2s <- c(kin2s, kin2)
+        p.vals <- c(p.vals, NA)
+        next
+    }
     kin1.cor.pairs <- which(!is.na(kin.cor[kin1,]))
     kin2.cor.pairs <- which(!is.na(kin.cor[kin2,]))
     shared.pairs <- intersect(kin1.cor.pairs, kin2.cor.pairs)
