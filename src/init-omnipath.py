0996eed0f45cf260cf0d3b2cdb98baa9a059ea0a
Brandon Invergo <brandon@invergo.net>
2018-01-24 14:44:21 +0000

    add back in all omnipath sources

--- src/init-omnipath.py
+++ src/init-omnipath.py
@@ -4,7 +4,7 @@ import pypath
 
 if __name__ == "__main__":
     pa = pypath.PyPath()
-    pa.init_network(exclude=["psite", "signor", "dbptm", "depod", "phelm"])
+    pa.init_network() #exclude=["psite", "signor", "dbptm", "depod", "phelm"])
     # remove high-throughput data
     pa.remove_htp()
     pa.get_directed()
