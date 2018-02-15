5b7da34ef5e2bb8cb364e206d71068207e05e511
Brandon Invergo <brandon@invergo.net>
2018-02-15 10:31:55 +0000

    filter psiteplus data by evidence

--- src/filter-psite-plus-tbl.r
+++ src/filter-psite-plus-tbl.r
@@ -8,10 +8,17 @@ if (length(argv) != 2){
 psite.plus.file <- argv[1]
 out.file <- argv[2]
 
-psite.plus.tbl <- read.delim(psite.plus.file, as.is=TRUE, sep="\t")
-kin.act <- log.wKSEA.kinase_condition.clean
+kinases <- read.table("data/human-kinome.txt", as.is=TRUE)[,1]
 
-psite.plus.filt <- subset(psite.plus.tbl, GENE %in% rownames(kin.act))
+psite.plus.tbl <- read.delim(psite.plus.file, as.is=TRUE, sep="\t")
+
+psite.plus.filt <- subset(psite.plus.tbl, GENE %in% kinases)
+
+if ("LT_LIT" %in% colnames(psite.plus.filt)){
+    psite.plus.filt$LT_LIT[which(is.na(psite.plus.filt$LT_LIT))] <- 0
+    psite.plus.filt$MS_LIT[which(is.na(psite.plus.filt$MS_LIT))] <- 0
+    psite.plus.filt <- subset(psite.plus.filt, LT_LIT+MS_LIT >= 5)
+}
 
 write.table(psite.plus.filt, out.file, sep="\t", quote=FALSE,
             row.names=FALSE, col.names=TRUE)
