5480abeec302453ad58a5ed2cace894583621bce
Brandon Invergo <brandon@invergo.net>
2018-02-23 15:54:18 +0000

    create missing directory as needed

--- do-ks.sh
+++ do-ks.sh
@@ -3,8 +3,10 @@
 NUM_CONDS=668
 LOGDIR=log/ks
 USE_AUTOPHOS=TRUE
+TMPDIR=tmp/ks-autophos
 
 mkdir -p $LOGDIR
+mkdir -p $TMPDIR
 
 bsub -M 8192 -R "rusage[mem=1024]" -o ${LOGDIR}/ks-%I.out -e ${LOGDIR}/ks-%I.err \
      -J "ks[1-${NUM_CONDS}]" Rscript src/kinact-ks.r '\$LSB_JOBINDEX' $USE_AUTOPHOS
