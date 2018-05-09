edd3a1d7d4062920757eb4d83810bb0298f776a7
Brandon Invergo <brandon@invergo.net>
2018-05-09 11:42:57 +0100

    also generate an undirected validation set from omnipath

--- src/gen-omnipath-valset.py
+++ src/gen-omnipath-valset.py
@@ -5,6 +5,20 @@ import os.path
 import pypath
 
 
+def get_assocs(pa, prot_list):
+    assocs = set()
+    for prot in prot_list:
+        neighbs = list(pa.gs_neighbors(prot).gs())
+        for neighb in neighbs:
+            if neighb == prot or neighb not in prot_list:
+                continue
+            srcs = pa.get_edge(prot, neighb)["sources"]
+            if len(srcs) < 2:
+                continue
+            assocs.add((prot, neighb))
+    return assocs
+
+
 def get_reg_rels(pa, prot_list):
     reg_rels = set()
     for prot in prot_list:
@@ -32,12 +46,16 @@ if __name__ == "__main__":
     kin_list_file = sys.argv[1]
     pa = pypath.PyPath()
     pa.init_network(pfile="cache/default_network.pickle")
-    pa.get_directed()
     kinases = set()
     with open(kin_list_file) as h:
         for line in h:
             kinases.add(line.strip())
+    assocs = get_assocs(pa, kinases)
+    with open("data/assoc-validation-set-omnipath.tsv", 'w') as h:
+        for assoc in assocs:
+            h.write("\t".join(assoc)+"\n")
+    pa.get_directed()
     reg_rels = get_reg_rels(pa, kinases)
-    with open("data/validation-set-omnipath.tsv", 'w') as h:
+    with open("data/direct-validation-set-omnipath.tsv", 'w') as h:
         for rel in reg_rels:
             h.write("\t".join(rel)+"\n")
