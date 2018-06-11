3f193ee18147110fad3cd4fa769ff56c94dabf7c
Borgthor Petursson <borgthor@ebi.ac.uk>
2018-06-11 15:15:12 +0000

    Upload New File

---
+++ src/do-clust.sh
@@ -0,0 +1,20 @@
+declare -a meth=("Louvain" "greedy") 
+declare -a types=("adj" "other")
+ts=$(seq 0.0 0.02 0.98)
+
+
+for m in "${meth[@]}"
+do
+echo $m
+	for ty in "$types"
+	do
+		echo $ty
+		for t in $ts
+		
+		do
+		echo $t
+		Rscript netw-clust.r full-annotated-predictor.tsv $t $m $ty  
+		done
+	done
+done
+		
