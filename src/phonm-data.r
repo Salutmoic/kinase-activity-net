df78e602a266b3c1b765d75822ac0295715b3f8f
Borgthor Petursson <borgthor@ebi.ac.uk>
2018-08-14 12:41:49 +0000

    Replace phonm-data.r

--- src/phonm-data.r
+++ src/phonm-data.r
@@ -1,64 +1,13 @@
-make.map = function(data,d.rows){
-		
-	map = matrix(ncol=7,nrow = nrow(data))
-	colnames(map) =  c("X", "dataID", "UPID", "site","res", "pos", "S.cc")
-	for(i in 1:nrow(data)){
-		site = strsplit(as.character(data[i,1])," ")[[1]][2]
-		site = gsub("p-","",site)
-		upid =paste(d.rows[i],"HUMAN",sep = "_")
-		row = c(i,as.character(data[i,1]),upid,site,substr(site,1,1),substr(site,2,nchar(site)),paste(upid,substr(site,1,1),substr(site,2,nchar(site)),sep = "."))	
-		map[i,] = row
-	}	
-	return( map)	
-}
-
-
-make.prior = function(netw,allD,uniprot,data,d.rows,map){
-	
-	uni.d = allD[,c(1,2)]
-	uni.p = allD[,c(3,4)]
-	colnames(uni.p) = colnames(uni.d)
-	uni = rbind(uni.d,uni.p)
-	prior = c()
-	m = 0
-	for(i in 1:nrow(netw)){
-		s.id = paste(as.character(netw[i,2]),"HUMAN",sep ="_")
-		if(s.id %in% as.character(uni[,2])){
-			s.ac = as.character(uni[which(as.character(uni[,2]) ==as.character(s.id))[1],1])
-		}else{
-			s.ac = as.character(uniprot[which(as.character(uniprot[,2]) == as.character(netw[i,2]))[1],1])
-		}
-		k.id = paste(as.character(netw[i,1]),"HUMAN",sep = "_")
-		if(k.id %in% as.character(uni[,2])){
-			k.ac = as.character(uni[which(as.character(uni[,2]) ==as.character(k.id))[1],1])
-		}else{
-			k.ac = as.character(uniprot[which(as.character(uniprot[,2]) == as.character(netw[i,1]))[1],1])
-		}
-		map.rows = data.frame(map[which(map$UPID == s.id),])
-		rows = c()
-		n = 1
-		for(h in 1:nrow(map.rows)){
-			r = c(s.ac,s.id,k.ac,k.id,as.character(map.rows[h,]$res),as.character(map.rows[h,]$pos),
-			paste("e",as.character(m+n),sep= ""),as.character(map.rows[h,]$S.cc))			
-			rows = rbind(rows,t(r))
-			n = n+1		
-		}		
-		prior = rbind(prior,rows)		
-		m= nrow(prior)
-	}
-	colnames(prior) = c("S.AC", "S.ID", "K.AC", "K.ID","res", "pos", "SID", "S.cc")
-	return(prior)
-}
+source("phon-dat-functions.r")
 
 argv <- commandArgs(TRUE)
 
-data = read.csv("Data_AND_Background_Network/log_measurments.csv")
-#regs = read.delim("Regulatory_sites_2018-05-01",skip = 2)
-#regs = regs[which(regs$ORGANISM =="human"),]
+t.a = argv[1]
+t.d = argv[2]
+status = argv[3]
 
-#mod.rsd = strsplit(as.character(regs$MOD_RSD),"-")
-#mrsd = lapply(mod.rsd,function(x) paste(x[2],x[1],sep = "-"))
-#rown = paste(regs$PROTEIN,mrsd)
+data = read.csv("Data_AND_Background_Network/log_measurments.csv")
+uniprot = read.delim("uniprot-id-map.tsv",header = F)
 
 prots = strsplit(as.character(data[,1])," ")
 m = lapply(prots,length)
@@ -68,44 +17,77 @@ inds = which(m > 4)
 ps = list()
 indexes = c()
 
-for(i in inds){
+if(status == "reg"){
+#Only inlcude regulatory phosphosites in data
+	regs = read.delim("Regulatory_sites_2018-05-01",skip = 2)
+	regs = regs[which(regs$ORGANISM =="human"),]
+
+	mod.rsd = strsplit(as.character(regs$MOD_RSD),"-")
+        mrsd = lapply(mod.rsd,function(x) paste(x[2],x[1],sep = "-"))
+	rown = paste(regs$PROTEIN,mrsd)
+
+	for(i in inds){
 	prot = prots[i]
 	n = 2
 	v = length(ps)
 	
 	while(length(grep("\\(",prots[[i]][n])) == 0){
-		psite = paste(prots[[i]][1],prots[[i]][n])
-		if(length(ps) ==v){	
-			ps[[length(ps)+1]] = psite
-		}else{
+			psite = paste(prots[[i]][1],prots[[i]][n])
+			if(length(ps) ==v){	
+				ps[[length(ps)+1]] = psite
+			}else{
 			#ps[[v+1]] = c(ps[[length(ps)]],psite)
-			ps[[length(ps)+1]] = psite
+				ps[[length(ps)+1]] = psite
+			}
+			n = n+1
+			if(psite %in% rown){
+				#indexes = c(indexes,i)
+			}
 		}
-		n = n+1
-		#if(ps %in% rown){
-		#indexes = c(indexes,i)
-		#}
+		indexes = c(indexes,i)
 	}
-	indexes = c(indexes,i)
+	
+	indexes = unique(indexes)
+	rows = rep(0,nrow(data))
+	rows[which(prots.m %in% rown)] =1
+	rows[indexes] = 1
+
+	data = data[which(rows==1),] 
+
 }
 
-prots.m = c(unlist(prots.m),unlist(ps))
-psites= read.delim("psites-to-use.tsv")
-uniprot = read.delim("uniprot-id-map.tsv",header = F)
+if(status == "func"){
+#Only consider top X% phosphosites in regards to function
+	psites= read.delim("psites-to-use.tsv")
+	psites = psites[psites[,2] > 0.2,]
+	psites.genes = paste(lapply(psites[,1],function(x) as.character(uniprot[which(as.character(uniprot[,1]) == gsub("_.*","",x)),2])),"_HUMAN",sep = "")
+	sites = lapply(psites[,1],function(x) strsplit(as.character(x),"_")[[1]][2])
+	for(i in inds){
+		prot = prots[i]
+		n = 2
+		v = length(ps)
+	
+		while(length(grep("\\(",prots[[i]][n])) == 0){
+			psite = paste(prots[[i]][1],prots[[i]][n])
+			if(length(ps) ==v){	
+				ps[[length(ps)+1]] = psite
+			}else{
+				#ps[[v+1]] = c(ps[[length(ps)]],psite)
+				ps[[length(ps)+1]] = psite
+			}
+			n = n+1
+			#if(ps %in% rown){
+				#indexes = c(indexes,i)
+			#}
+		}
+		indexes = c(indexes,i)
+	}
 
-psites.genes = paste(lapply(psites[,1],function(x) as.character(uniprot[which(as.character(uniprot[,1]) == gsub("_.*","",x)),2])),"_HUMAN",sep = "")
-sites = lapply(psites[,1],function(x) strsplit(as.character(x),"_")[[1]][2])
+	prots.m = c(unlist(prots.m),unlist(ps))
+}
 
-#indexes = unique(indexes)
-#rows = rep(0,nrow(data))
-#rows[which(prots.m %in% rown)] =1
-#rows[indexes] = 1
-
-#data = data[which(rows==1),] 
 
 netw = read.delim("separated-predictor-annot.tsv")
-t.a = argv[1]
-t.d = argv[2]
 kins = unique(as.character(netw[,1]))
 
 netw = netw[which(netw[,3] > t.a & netw$direct.pred.mean > t.d),]
@@ -118,7 +100,10 @@ d.rows = d.rows[which(d.rows %in% kins)]
 
 my.map = make.map(data,d.rows)
 my.map = as.data.frame(my.map)
-my.map = my.map[which(paste(my.map$UPID,my.map$pos) %in% paste(psites.genes, sites)),]
+
+if(status == "func"){
+	my.map = my.map[which(paste(my.map$UPID,my.map$pos) %in% paste(psites.genes, sites)),]
+}
 
 data = data[data[,1] %in% my.map$dataID,]
 d.rows = strsplit(as.character(data[,1])," ")
@@ -126,12 +111,27 @@ d.rows = lapply(d.rows,function(x) x[1])
 
 netw = netw[which(netw[,2] %in% d.rows),]
 
-write.table(my.map,"my-map-func.tsv",quote= F ,sep = "\t",row.names = F)
 load("Data_AND_Background_Network/allD_HUMAN_Omnipath.Rdata")
 prior = make.prior(netw,allD,uniprot,data,d.rows,my.map)
 prior = as.data.frame(prior)
-save("prior",file= "prior-matrix-func.Rdata")
 allD = allD[which(allD$S.cc %in% my.map$S.cc),]
-save("allD",file = "omni-prior-func.Rdata")
 
-write.csv(data,"log-subset-func.csv",row.names = F)
+
+
+if(status == "func"){
+	write.table(my.map,"my-map-func.tsv",quote= F ,sep = "\t",row.names = F)
+	write.csv(data,"log-subset-func.csv",row.names = F)
+	save("allD",file = "omni-prior-func.Rdata")
+	save("prior",file= "prior-matrix-func.Rdata")
+}else if(status == "reg"){
+	write.table(my.map,"my-map-reg.tsv",quote= F ,sep = "\t",row.names = F)
+	write.csv(data,"log-subset-reg.csv",row.names = F)
+	save("allD",file = "omni-prior-reg.Rdata")
+	save("prior",file= "prior-matrix-reg.Rdata")
+}else{
+	write.table(my.map,"my-map.tsv",quote= F ,sep = "\t",row.names = F)
+	write.csv(data,"log-subset.csv",row.names = F)
+	save("allD",file = "omni-prior.Rdata")
+	save("prior",file= "prior-matrix.Rdata")
+}
+
